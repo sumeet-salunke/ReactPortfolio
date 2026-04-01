e8c6391e175cbb6d2aaaf09020b66d84179d776c
Sumeet Salunke <sumeetsuprit4444@gmail.com>
2026-04-01 18:41:35 +0530

    Added two new projects

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -5,9 +5,60 @@ import JobApplicationApp from '../assets/images/ApplicationTracker.png';
 import CampusCart from '../assets/images/CampusCart.png';
 import MovieSearchApp from '../assets/images/movieApp.png';
 import WeatherApp from '../assets/images/WeatherApp.png';
+import MiniJira from "../assets/images/MiniJira.png";
+import ExpenseTracker from "../assets/images/ExpenseTracker.png";
 const projectsData = [
   {
     id: 1,
+    title: "Mini Jira – Full Stack Task Management System",
+    shortDesc: "Full stack project management application with authentication, projects, tasks, and dashboard analytics",
+    tech: ["React", "Node.js", "Express", "MongoDB", "JWT", "Axios", "GitHub Pages", "Render"],
+    description:
+      "Mini Jira is a full stack task management web application where users can register, login, create projects, manage tasks, update task status, and add comments on tasks. The project focuses on full stack architecture, REST APIs, authentication using JWT, database relationships, and deploying frontend and backend separately. The frontend is built using React and deployed on GitHub Pages, while the backend API is built with Node.js and Express and deployed on Render with MongoDB Atlas database.",
+    learnings: [
+      "Designing backend architecture using MVC pattern",
+      "Creating REST APIs with Node.js and Express",
+      "Implementing authentication using JWT",
+      "Designing database relationships between users, projects, tasks, and comments",
+      "Connecting React frontend with backend APIs using Axios",
+      "Handling protected routes and token-based authentication",
+      "Deploying backend on Render and frontend on GitHub Pages",
+      "Managing environment variables and CORS for production deployment",
+      "Understanding full stack application flow from frontend to database"
+    ],
+    demoLink: "",
+    LiveLink: "https://sumeet-salunke.github.io/Mini_Jira_FullStack",
+    backendAPI: "https://mini-jira-backend.onrender.com",
+    githubFrontend: "https://github.com/sumeet-salunke/Mini_Jira_FullStack",
+    githubBackend: "https://github.com/sumeet-salunke/Mini_Jira_FullStack",
+    image: MiniJira,
+    linkedin: "https://www.linkedin.com/posts/sumeet-salunke-052070325_fullstack-react-nodejs-ugcPost-7445091428483076096-aghl?utm_source=share&utm_medium=member_desktop&rcm=ACoAAFIOhz4BkQ5ELiUbUUaI0PEXZQ39ek5rl30"
+  },
+  {
+    id: 2,
+    title: "SumeetsCampusCart",
+    shortDesc: "Full-stack campus marketplace application",
+    tech: ["React", "Node.js", "Express", "MongoDB", "Session Auth"],
+    description:
+      "A full-stack marketplace web application inspired by platforms like Amazon/Myntra where users can browse products, add items to cart, and place orders. The project focuses on implementing session-based authentication, role-based access control, and deploying a production-ready MERN application.",
+    learnings: [
+      "Session-based authentication using cookies",
+      "Role-based access control (Admin/User)",
+      "Handling CORS and cross-domain cookies",
+      "Debugging production issues like 304 caching and headers",
+      "Deploying full-stack apps (Frontend → Netlify, Backend → Render)",
+      "Fixing React Router routing issues on Netlify using _redirects"
+    ],
+    demoLink: "",
+    LiveLink: "https://sumeetscampuscart.netlify.app",
+    backendAPI: "https://campuscart-api.onrender.com/api/products",
+    githubFrontend: "https://github.com/Vidhan4444/campuscart-frontend",
+    githubBackend: "https://github.com/Vidhan4444/campuscart-backend",
+    image: CampusCart,
+    linkedin: "https://www.linkedin.com/posts/sumeet-salunke-052070325_mernstack-fullstackdevelopment-reactjs-share-7435351918018818048-ly29?utm_source=share&utm_medium=member_desktop&rcm=ACoAAFIOhz4BkQ5ELiUbUUaI0PEXZQ39ek5rl30"
+  },
+  {
+    id: 3,
     title: "Movie Search App",
     shortDesc: "JavaScript movie search application using a public API",
     tech: ["HTML", "CSS", "JavaScript", "Fetch API", "Open API"],
@@ -31,7 +82,7 @@ const projectsData = [
   }
   ,
   {
-    id: 2,
+    id: 4,
     title: "Weather App",
     shortDesc: "JavaScript weather application using Open-Meteo APIs",
     tech: ["HTML", "CSS", "JavaScript", "Fetch API", "Open-Meteo API"],
@@ -54,7 +105,7 @@ const projectsData = [
     linkedin: "https://www.linkedin.com/posts/sumeet-salunke-052070325_javascript-webdevelopment-frontenddevelopment-share-7440000244521684992--wlK?utm_source=share&utm_medium=member_desktop&rcm=ACoAAFIOhz4BkQ5ELiUbUUaI0PEXZQ39ek5rl30"
   },
   {
-    id: 3,
+    id: 5,
     title: "Reminder App",
     shortDesc: "Email-based reminder application",
     tech: ["React", "Node.js", "MongoDB"],
@@ -70,7 +121,7 @@ const projectsData = [
     linkedin: 'https://www.linkedin.com/posts/sumeet-salunke-052070325_reactjs-fullstackdevelopment-webdevelopment-activity-7416822059336458240-XB0I?utm_source=social_share_send&utm_medium=member_desktop_web&rcm=ACoAAFIOhz4BkQ5ELiUbUUaI0PEXZQ39ek5rl30'
   },
   {
-    id: 4,
+    id: 6,
     title: "Portfolio & Resume Generator",
     shortDesc: "Generate a portfolio and resume using predefined templates",
     tech: ["HTML", "CSS", "Node.js", "MongoDB"],
@@ -87,7 +138,7 @@ const projectsData = [
     linkedin: 'https://www.linkedin.com/posts/sumeet-salunke-052070325_webdevelopment-miniproject-frontend-activity-7404814559099785218-OEQK?utm_source=social_share_send&utm_medium=member_desktop_web&rcm=ACoAAFIOhz4BkQ5ELiUbUUaI0PEXZQ39ek5rl30'
   }
   , {
-    id: 5,
+    id: 7,
     title: "Voice-Based Assistant",
     shortDesc: "Performs tasks based on user voice commands using Python",
     tech: ["Python", "SpeechRecognition", "pyttsx3"],
@@ -104,7 +155,7 @@ const projectsData = [
     linkedin: 'https://www.linkedin.com/posts/sumeet-salunke-052070325_python-automation-learningbydoing-activity-7408743975978799104-L3nd?utm_source=social_share_send&utm_medium=member_desktop_web&rcm=ACoAAFIOhz4BkQ5ELiUbUUaI0PEXZQ39ek5rl30'
   },
   {
-    id: 6,
+    id: 8,
     title: "Job Application Management App",
     shortDesc: "Secure CRUD app for managing job applications",
     tech: ["React", "Node.js", "Express", "MongoDB", "JWT"],
@@ -124,27 +175,27 @@ const projectsData = [
   }
   ,
   {
-    id: 7,
-    title: "SumeetsCampusCart",
-    shortDesc: "Full-stack campus marketplace application",
-    tech: ["React", "Node.js", "Express", "MongoDB", "Session Auth"],
+    id: 9,
+    title: "Expense Tracker",
+    shortDesc: "Expense tracking web application to manage income and expenses",
+    tech: ["HTML", "CSS", "JavaScript", "LocalStorage"],
     description:
-      "A full-stack marketplace web application inspired by platforms like Amazon/Myntra where users can browse products, add items to cart, and place orders. The project focuses on implementing session-based authentication, role-based access control, and deploying a production-ready MERN application.",
+      "An expense tracking web application that allows users to add income and expense transactions, view balance, and track financial history. The project focuses on DOM manipulation, state management on the frontend, and storing data in browser LocalStorage to persist transactions without a backend.",
     learnings: [
-      "Session-based authentication using cookies",
-      "Role-based access control (Admin/User)",
-      "Handling CORS and cross-domain cookies",
-      "Debugging production issues like 304 caching and headers",
-      "Deploying full-stack apps (Frontend → Netlify, Backend → Render)",
-      "Fixing React Router routing issues on Netlify using _redirects"
+      "DOM manipulation and event handling in JavaScript",
+      "Managing application state on the frontend",
+      "Using LocalStorage for data persistence",
+      "Form handling and input validation",
+      "Dynamic UI updates based on data",
+      "Structuring small frontend projects properly"
     ],
     demoLink: "",
-    LiveLink: "https://sumeetscampuscart.netlify.app",
-    backendAPI: "https://campuscart-api.onrender.com/api/products",
-    githubFrontend: "https://github.com/Vidhan4444/campuscart-frontend",
-    githubBackend: "https://github.com/Vidhan4444/campuscart-backend",
-    image: CampusCart,
-    linkedin: "https://www.linkedin.com/posts/sumeet-salunke-052070325_mernstack-fullstackdevelopment-reactjs-share-7435351918018818048-ly29?utm_source=share&utm_medium=member_desktop&rcm=ACoAAFIOhz4BkQ5ELiUbUUaI0PEXZQ39ek5rl30"
+    LiveLink: "https://sumeet-salunke.github.io/expense-tracker/",
+    backendAPI: "",
+    githubFrontend: "",
+    githubBackend: "",
+    image: ExpenseTracker,
+    linkedin: "https://www.linkedin.com/posts/sumeet-salunke-052070325_javascript-webdevelopment-frontenddevelopment-activity-7440433362558672896-DAO6?utm_source=share&utm_medium=member_desktop&rcm=ACoAAFIOhz4BkQ5ELiUbUUaI0PEXZQ39ek5rl30"
   }
 
 ];
